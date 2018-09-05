8e65642335e7b479fe7fc85eef33345010e8a29c
AlinaKr <mirgehendieideenaus@gmail.com>
2018-09-05 10:35:13 +0200

    favorites working including remove features

--- routes/index.js
+++ routes/index.js
@@ -29,38 +29,52 @@ router.get("/free-list", (req, res, next) => {
 router.get('/profile', (req, res, next) => {
   User.findById(req.user._id)
     .then(userFromDb => {
-      console.log(userFromDb)
+      //console.log(userFromDb)
       //userFromDb.populate(_contributions)
       //console.log(userFromDb._contributions)
       Resource.find({ _owner: req.user._id })
         .then(resourcesFromOwner => {
           var resourcesFromOwner = resourcesFromOwner
-          //console.log("resourcesfrmownerfirst", resourcesFromOwner)
-          Favorite.find({ _owner: req.user._id })
-            .populate('_resource')
-            .then(faved => {
-              console.log("faved1", faved)
-              // if (faved) {
-              //   var favorites = faved;
-              // }
-              // else {
-              //   var favorites = [];
-              // }
-              //console.log("resourcefromowenr", resourcesFromOwner)
-              console.log("faved", faved)
-              res.render('profile', {
-                username: userFromDb.username,
-                password: userFromDb.password,
-                //favorized: amountLikes,  //userFromDb.favorized,
-                description: userFromDb.description,
-                contributions: userFromDb._contributions,
-                profilepic: userFromDb.profilepic,
-                createdAt: userFromDb.created_at,
-                contributions: resourcesFromOwner,
-                favorites: faved
-              })
-
+          // var counterLikes;
+          // resourcesFromOwner.forEach(function (resource) {   //lacking then here?
+          //   counterLikes += Favorite.find({ _resource: resource._id }).length
+          //   console.log("FAVORITEFIND____", Favorite.find({ _resource: resource._id }))
+          // })
+          let promises = [];
+          for (var i = 0; i < resourcesFromOwner.length; i++) {
+            promises.push(Favorite.count({ _resource: resourcesFromOwner[i]._id }))
+          }
+          Promise.all(promises)
+            .then(counterLikes => {
+              if (counterLikes.length > 0) {
+                var counterLikes = counterLikes.reduce(function (total, amount) {
+                  return total + amount;
+                })
+              }
+              else {
+                var counterLikes = 0;
+              }
+              //console.log(counterLikes, "counterLikes")
+              //console.log("resourcesfrmownerfirst", resourcesFromOwner)
+              Favorite.find({ _owner: req.user._id })
+                .populate('_resource')
+                .then(faved => {
+                  //console.log("faved1", faved)
+                  //console.log("faved", faved)
+                  res.render('profile', {
+                    username: userFromDb.username,
+                    password: userFromDb.password,
+                    favorized: counterLikes,  //userFromDb.favorized,
+                    description: userFromDb.description,
+                    contributions: userFromDb._contributions,
+                    profilepic: userFromDb.profilepic,
+                    createdAt: userFromDb.created_at,
+                    contributions: resourcesFromOwner,
+                    favorites: faved
+                  })
+                })
             })
+
 
 
         });
@@ -73,8 +87,8 @@ router.get("/add", (req, res, next) => {
 });
 
 router.post("/add", (req, res, next) => {
-  console.log("req.body", req.body);
-  console.log("req.user", req.user);
+  //console.log("req.body", req.body);
+  //console.log("req.user", req.user);
   const newResource = new Resource({
     category: req.body.category,
     shortdescr: req.body.shortdescr,
@@ -140,6 +154,26 @@ router.post('/:id/edit', (req, res, next) => {
     .catch(next)
 });
 
+router.get('/editProfile', (req, res, next) => {
+  User.findById(req.user.id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).render('not-found');
+      }
+      res.render("edit-profile", user)
+    })
+    .catch(next)
+})
+
+router.post('/editProfile', (req, res, next) => {
+  const { username, description, profilepic } = req.body;
+  User.update({ _id: req.user.id }, { $set: { username, description, profilepic } }, { new: true })
+    .then((user) => {
+      res.redirect('/profile')
+    })
+    .catch(next)
+});
+
 // router.post('/:id/favorize', (req, res, next) => {
 //   ////
 //   //res.redirect('/:id/detail')
@@ -148,29 +182,84 @@ router.post('/:id/edit', (req, res, next) => {
 router.get('/:id/favorize', (req, res, next) => {
   ////
   //res.redirect('/:id/detail')
+  console.log("XXXXXXXXI am here");
   ResourceId = req.params.id;
   UserId = req.user._id;
   const newFavorite = new Favorite({
     _owner: UserId,
     _resource: ResourceId
   })
-  newFavorite.save()
-    .then((fav) => {
-      console.log("The fav was saved!!!", fav);
-      Resource.findById(req.params.id)
-        .then(resourcesFromDb => {
-          res.render('detail', {
-            category: resourcesFromDb.category,
-            shortdescr: resourcesFromDb.shortdescr,
-            longdescr: resourcesFromDb.longdescr,
-            location: resourcesFromDb.location,
-            date: resourcesFromDb.date,
-            image: resourcesFromDb.image,
-            _id: req.params.id
-          });
+  Favorite.find({ _owner: req.user._id, _resource: req.params.id })
+    .then(favorited => {
+      if (favorited.length > 0) {
+        ///dont save in favs
+        console.log("LLLLLLLhas already been faved")
+        console.log(favorited, "XXXXXXXXXXXXXfavorited")
+        Favorite.deleteOne({ _owner: req.user._id, _resource: req.params.id })
+          .then(removedFav => {
+            Resource.findById(req.params.id)
+              .then(resourcesFromDb => {
+                res.render('detail', {
+                  category: resourcesFromDb.category,
+                  shortdescr: resourcesFromDb.shortdescr,
+                  longdescr: resourcesFromDb.longdescr,
+                  location: resourcesFromDb.location,
+                  date: resourcesFromDb.date,
+                  image: resourcesFromDb.image,
+                  _id: req.params.id,
+                  color: "background-color:grey"
+                });
+              })
+          })
+      }
+      else {
+        newFavorite.save()
+          .then((fav) => {
+            console.log("XXXXXXThe fav was saved!!!", fav);
+            Resource.findById(req.params.id)
+              .then(resourcesFromDb => {
+                res.render('detail', {
+                  category: resourcesFromDb.category,
+                  shortdescr: resourcesFromDb.shortdescr,
+                  longdescr: resourcesFromDb.longdescr,
+                  location: resourcesFromDb.location,
+                  date: resourcesFromDb.date,
+                  image: resourcesFromDb.image,
+                  _id: req.params.id,
+                  color: "color:red"
+                });
+              })
+          })
+      }
+    })
+
+    .catch(next)
+});
+
+
+router.post('/:id/delete', (req, res, next) => {
+  Resource.findByIdAndRemove(req.params.id)
+    .then(resourcefromDb => {
+      Favorite.deleteMany({ _resource: req.params.id })
+        .then(deleted => {
+          res.redirect('/profile')
         })
     })
     .catch(next)
 });
+
+router.post('/favorites/:resourceId/delete', (req, res, next) => {
+  console.log("REQPARAMS", req.params)
+  Favorite.deleteOne({ _resource: req.params.resourceId, _owner: req.user._id })
+    .then(favoritefromDb => {
+      res.redirect('/profile')
+    })
+    .catch(next)
+});
+
+// router.get("/favorite/:resourceID/create", (req, res) => {
+//   console.log("CREATING A FAVORITE!!!!!!!")
+//   res.send("HEY!!!!! CREATING A FAVORITE!!!!!!!")
+// })
 
 module.exports = router;
