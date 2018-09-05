965c5bf87403fa5618df62c8fc76640c71ceb67a
Samuele Maurizi <samuelemaurizi@gmail.com>
2018-09-05 11:49:29 +0200

    few views

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+  $('#selec-menu').each(function () {
+    $(this).children('option:first').attr('hidden','hidden');
+  });  
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
