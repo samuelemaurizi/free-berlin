767d47bb35948cc78a9cfd525ff27003996577ff
Samuele Maurizi <samuelemaurizi@gmail.com>
2019-12-26 16:56:45 +0100

    adjust some style and mobile version

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-  $('#selec-menu').each(function () {
-    $(this).children('option:first').attr('hidden','hidden');
-  });  
-  console.log('IronGenerator JS imported successfully!');
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    $('#selec-menu').each(function() {
+      $(this)
+        .children('option:first')
+        .attr('hidden', 'hidden');
+    });
+    console.log('IronGenerator JS imported successfully!');
+  },
+  false
+);
 
-}, false);
+function openNav() {
+  document.getElementById('mobile__nav').style.width = '100%';
+}
+
+function closeNav() {
+  document.getElementById('mobile__nav').style.width = '0';
+}
